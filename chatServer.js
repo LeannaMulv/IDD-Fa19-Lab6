448c9f3317f63ed44de66448855a38e220e81564
LeannaMulv <40302225+LeannaMulv@users.noreply.github.com>
2019-10-21 09:13:58 -0400

    Update chatServer.js

--- chatServer.js
+++ chatServer.js
@@ -138,9 +138,9 @@ function bot(data, socket, questionNum) {
 function timedQuestion(socket, question) {
   if (question != '') {
     socket.emit('question', question);
-  } else {
+  } //else {
     //console.log('No Question send!');
-  }
+  //}
 
 
 //----------------------------------------------------------------------------//
